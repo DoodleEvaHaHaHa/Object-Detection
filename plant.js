50b861e2dec28a7759c2934522e65fd8c9cb0c4c
DoodleEvaHaHaHa <92247115+DoodleEvaHaHaHa@users.noreply.github.com>
2022-05-24 13:59:40 +0530

    Add files via upload

---
+++ plant.js
@@ -0,0 +1,37 @@
+img = "";
+Status = "";
+
+function preload()
+{
+    img = loadImage("plant image.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(640, 360);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, result)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(result);
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Plants", 150, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(135, 60, 420, 310)
+}
